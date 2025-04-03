97535345857d872f1bcff85e400e6ec7b2fbbbbe
文森特 <wstntgg@gmail.com>
2025-04-03 20:55:26 +0200

    Running the server, creationg of the HTML file to see on the running server

--- main.go
+++ main.go
@@ -7,17 +7,22 @@ import (
 )
 
 func main() {
+	const port = "8080"
+	const filepathRoot = "."
+
 	mux := http.NewServeMux()
 
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
 	srv := &http.Server{
-		Addr:         ":8080",
+		Addr:         ":" + port,
 		Handler:      mux,
 		IdleTimeout:  60 * time.Second,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
-	log.Println("Server is running on port 8080")
+	log.Println("Server is running on port " + port)
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
