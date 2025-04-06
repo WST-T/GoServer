e33056de4751d29c3affc9d266e607bab44bb2c6
文森特 <wstntgg@gmail.com>
2025-04-06 17:52:20 +0200

    Setting up database connection & SQLC

--- main.go
+++ main.go
@@ -1,25 +1,52 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
 	"time"
+
+	"github.com/WST-T/GoServer/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	DB             *database.Queries
 }
 
 func main() {
-	const port = "8080"
+
 	const filepathRoot = "."
 
-	mux := http.NewServeMux()
+	godotenv.Load(".env")
 
-	apiCfg := apiConfig{
-		fileserverHits: atomic.Int32{},
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("$PORT must be set")
 	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("$DB_URL must be set")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Cant't connect to the database", err)
+	}
+
+	db := database.New(conn)
+
+	apiCfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+		DB:             db,
+	}
+
+	mux := http.NewServeMux()
 
 	handler := http.FileServer(http.Dir(filepathRoot))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", handler)))
@@ -30,15 +57,15 @@ func main() {
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
 
 	srv := &http.Server{
-		Addr:         ":" + port,
+		Addr:         ":" + portString,
 		Handler:      mux,
 		IdleTimeout:  60 * time.Second,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
-	log.Println("Server is running on port " + port)
-	err := srv.ListenAndServe()
+	log.Println("Server is running on port " + portString)
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
